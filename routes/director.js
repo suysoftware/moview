0b14108cc8b985e0c009a9b6f94e8d6362366e95
SUY <suy.register@gmail.com>
2023-03-01 19:01:03 +0300

    /api/directors/:director_id GET method

--- routes/director.js
+++ routes/director.js
@@ -1,3 +1,4 @@
+const mongoose=require('mongoose');
 const express = require('express');
 const router = express.Router();
 
@@ -60,6 +61,59 @@ router.get('/', (req, res) => {
 		res.json(err);
 	});
 });
+
+router.get('/:director_id', (req, res) => {
+	const promise = Director.aggregate([
+
+		{
+			$match:{
+				'_id':new mongoose.Types.ObjectId(req.params.director_id)
+			}
+		},
+		{
+			$lookup: {
+				from: 'movies',
+				localField: '_id',
+				foreignField: 'director_id',
+				as: 'movies'
+			}
+		},
+		{
+			$unwind: {
+				path: '$movies',
+				preserveNullAndEmptyArrays: true
+			}
+		},
+		{
+			$group: {
+				_id: {
+					_id: '$_id',
+					name: '$name',
+					surname: '$surname',
+					bio: '$bio'
+				},
+				movies: {
+					$push: '$movies'
+				}
+			}
+		},
+		{
+			$project: {
+				_id: '$_id._id',
+				name: '$_id.name',
+				surname: '$_id.surname',
+				movies: '$movies'
+			}
+		}
+	]);
+
+	promise.then((data) => {
+		res.json(data);
+	}).catch((err) => {
+		res.json(err);
+	});
+});
+
 
 
 module.exports = router;
