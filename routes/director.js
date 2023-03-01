4e7dd775c37cf930cd0f851aa7b4575740c31f2c
SUY <suy.register@gmail.com>
2023-03-01 17:49:02 +0300

    /api/directors POST Method

---
+++ routes/director.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+
+// Models
+const Director=require('../models/Director');
+
+router.post('/', function(req, res, next) {
+const director=new Director(req.body);
+const promise=director.save();
+promise.then((data)=>{
+    res.json(data);
+}).catch((err)=>{
+    res.json(err);
+});
+});
+
+module.exports = router;
