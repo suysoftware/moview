c5eb0dd302aedfd6b3c96c56468ff7430e0ba4c0
SUY <suy.register@gmail.com>
2023-03-01 18:56:30 +0300

    /api/directors GET Method

--- models/Director.js
+++ models/Director.js
@@ -3,13 +3,25 @@ const Schema=mongoose.Schema;
 
 
 const DirectorSchema=new Schema({
-name:String,
-surname:String,
-bio:String,
-createdAt:{
-    type:Date,
-    default:Date.now
-}
+	name: {
+		type: String,
+		maxlength: 60,
+		minlength: 2
+	},
+	surname: {
+		type: String,
+		maxlength: 60,
+		minlength: 2
+	},
+	bio: {
+		type: String,
+		maxlenght: 1000,
+		minlength: 60
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now
+	}
 
     
 });
--- routes/director.js
+++ routes/director.js
@@ -3,16 +3,64 @@ const router = express.Router();
 
 
 // Models
-const Director=require('../models/Director');
+const Director = require('../models/Director');
 
-router.post('/', function(req, res, next) {
-const director=new Director(req.body);
-const promise=director.save();
-promise.then((data)=>{
-    res.json(data);
-}).catch((err)=>{
-    res.json(err);
+router.post('/', function (req, res, next) {
+    const director = new Director(req.body);
+    const promise = director.save();
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
 });
+
+router.get('/', (req, res) => {
+	const promise = Director.aggregate([
+		{
+			$lookup: {
+				from: 'movies',
+				localField: '_id',
+				foreignField: 'director_id',
+				as: 'movies'
+			}
+		},
+		{
+			$unwind: {
+				path: '$movies',
+				preserveNullAndEmptyArrays: true
+			}
+		},
+		{
+			$group: {
+				_id: {
+					_id: '$_id',
+					name: '$name',
+					surname: '$surname',
+					bio: '$bio'
+				},
+				movies: {
+					$push: '$movies'
+				}
+			}
+		},
+		{
+			$project: {
+				_id: '$_id._id',
+				name: '$_id.name',
+				surname: '$_id.surname',
+				movies: '$movies'
+			}
+		}
+	]);
+
+	promise.then((data) => {
+		res.json(data);
+	}).catch((err) => {
+		res.json(err);
+	});
 });
+
 
 module.exports = router;
+
