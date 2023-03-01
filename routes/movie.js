ec3efe1b8ab423a44494d2fdbd21e5e5e014baf2
SUY <suy.register@gmail.com>
2023-03-01 16:52:01 +0300

    /movies/between endpoint

--- routes/movie.js
+++ routes/movie.js
@@ -2,30 +2,125 @@ const express = require('express');
 const router = express.Router();
 
 //Models
-const Movie=require('../models/Movie');
+const Movie = require('../models/Movie');
 
-router.post('/', (req, res, next) =>{
- const {title,imdb_score,category,country,year}=req.body;
+router.get('/', (req, res) => {
+  const promise = Movie.find({});
 
-const movie=new Movie({
-  title:title,
-  imdb_score:imdb_score,
-  category:category,
-  country:country,
-  year:year
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+// Top 10 list
+router.get('/top10', (req, res) => {
+  const promise = Movie.find({}).sort({imdb_score:-1}).limit(10);
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+router.get('/:movie_id', (req, res,next) => {
+  const promise = Movie.findById(req.params.movie_id);
+  promise.then((movie) => {
+
+    if(!movie)
+    next({message:'The movie has not found ',code:99});
+
+
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  });
 });
 
-const promise= movie.save();
-promise.then((data)=>{
-  res.json({status:1});
+router.put('/:movie_id',(req,res,next)=>{
+  const promise=Movie.findByIdAndUpdate(
+    req.params.movie_id,
+    req.body,
+    {new:true} //güncellenen data gelmesi için
+    );
 
-}).catch((err)=>{
-  res.json(err);
+  promise.then((movie) => {
+
+    if(!movie)
+    next({message:'The movie has not found ',code:99});
+
+
+    res.json({status:1});
+  }).catch((err) => {
+    res.json(err);
+  });
+
 });
 
 
-
+router.delete('/:movie_id',(req,res,next)=>{
+  const promise=Movie.findByIdAndRemove(
+    req.params.movie_id,
  
+    );
+
+  promise.then((movie) => {
+
+    if(!movie)
+    next({message:'The movie has not found ',code:99});
+
+
+    res.json({status:1});
+  }).catch((err) => {
+    res.json(err);
+  });
+
+});
+
+
+
+
+router.post('/', (req, res, next) => {
+  const { title, imdb_score, category, country, year } = req.body;
+
+  const movie = new Movie({
+    title: title,
+    imdb_score: imdb_score,
+    category: category,
+    country: country,
+    year: year
+  });
+
+  const promise = movie.save();
+  promise.then((data) => {
+    res.json({ status: 1 });
+
+  }).catch((err) => {
+    res.json(err);
+  });
+
+
+
+
+});
+
+
+// Between
+router.get('/between/:start_year/:end_year', (req, res) => {
+  const {start_year,end_year}=req.params;
+  const promise = Movie.find(
+    {
+      year:{"$gte":parseInt(start_year) ,"$lte":parseInt(end_year)}
+    }
+
+    
+    );
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
 });
 
 
