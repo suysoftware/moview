318477fa43d309c2a8738b1462de446603ed6e73
SUY <suy.register@gmail.com>
2023-03-01 19:07:54 +0300

    /api/directors/:director_id DELETE method

--- routes/director.js
+++ routes/director.js
@@ -132,7 +132,24 @@ router.put('/:director_id',(req,res,next)=>{
 	});
   
   });
-
+router.delete('/:director_id',(req,res,next)=>{
+	const promise=Director.findByIdAndRemove(
+	  req.params.director_id,
+   
+	  );
+  
+	promise.then((director) => {
+  
+	  if(!director)
+	  next({message:'The director has not found ',code:99});
+  
+  
+	  res.json({status:1});
+	}).catch((err) => {
+	  res.json(err);
+	});
+  
+  });
 
 module.exports = router;
 
