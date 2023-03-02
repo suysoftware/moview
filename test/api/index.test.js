4390360d9b451b96e4a9e9db469c2ec613842b76
SUY <suy.register@gmail.com>
2023-03-02 11:49:24 +0300

    mocha test script

---
+++ test/api/index.test.js
@@ -0,0 +1,19 @@
+const chai=require('chai');
+const chaiHttp=require('chai-http');
+const should=chai.should();
+const server=require('../../app');
+
+chai.use(chaiHttp);
+
+describe('Node Server',()=>{
+    it('(Get /) anasayfayı döndürür',(done)=>{
+
+        chai.request(server).get('/').end((err,res)=>{
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it('(Get /) movies endpoint',(done)=>{
+        done();
+    });
+});
